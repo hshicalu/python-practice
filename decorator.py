b7f5466ed3bcabc3d2ba5dc727e085c86ba5e76a
hicalu <hshicalu@gmail.com>
2022-10-26 14:16:15 +0900

    update decorator

--- decorator.py
+++ decorator.py
@@ -1,36 +1,16 @@
 
-class Coordinate(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def __repr__(self):
-        return 'Coord : ' + str(self.__dict__)
+def logger(func):
+    def inner(*args, **kwargs):
+        print('Arguments were : %s, %s' % (args, kwargs))
+        return func(*args, **kwargs)
+    return inner
 
-def wrapper(func):
-    def checker(a, b):
-        if a.x < 0 or a.y < 0:
-            a = Coordinate(a.x if a.x > 0 else 0, a.y if a.y > 0 else 0)
-        if b.x < 0 or b.y < 0:
-            b = Coordinate(b.x if b.x > 0 else 0, b.y if b.y > 0 else 0)
-        ret = func(a, b)
-        if ret.x < 0 or ret.y < 0:
-            ret = Coordinate(ret.x if ret.x > 0 else 0, ret.y if ret.y > 0 else 0)
-        return ret
-    return checker
+@logger
+def foo1(x, y=1):
+    return x * y
 
-@wrapper
-def add(a, b):
-    return Coordinate(a.x + b.x, a.y + b.y)
-@wrapper
-def sub(a, b):
-    return Coordinate(a.x - b.x, a.y - b.y)
+@logger
+def foo2():
+    return 2
 
-one = Coordinate(100, 200)
-two = Coordinate(300, 200)
-three = Coordinate(-100, -100)
-# print(add(one, two))
-# add = wrapper(add)
-# sub = wrapper(sub)
-
-print(sub(one, two))
-print(add(one, three))
+print(foo2())
