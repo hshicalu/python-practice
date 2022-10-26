96bc52916c72294dfee85dae7d2851564911f5ce
hicalu <hshicalu@gmail.com>
2022-10-26 13:35:17 +0900

    check func object

--- decorator.py
+++ decorator.py
@@ -1,5 +1,13 @@
 
-def foo():
-    pass
+def add(x, y):
+    return x + y
 
-print(issubclass(foo.__class__, object))
+def sub(x, y):
+    return x - y
+
+def apply(func, x, y):
+    return func(x, y)
+
+print(apply(add, 2, 1))
+
+print(apply(sub, 2, 1))
